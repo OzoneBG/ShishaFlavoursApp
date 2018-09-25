364660d62e7a134781e594e92542bc6de298ea0e
EZBOB\mihail.uzunov <mihail.uzunov@ezbob.com>
2018-09-25 17:32:11 +0300

    Initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 
+import { ContactComponent } from './contact/contact.component';
+import { CombinationComponent } from './combinations/combinations.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CombinationComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'contact',
+    template: 'Contact Component Hi'
+})
+
+export class ContactComponent { }
