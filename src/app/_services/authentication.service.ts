fe1efa536ee801322bc1adb155a0aa68cc3a2d5e
EZBOB\mihail.uzunov <mihail.uzunov@ezbob.com>
2018-10-22 09:56:19 +0300

    login implementation

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -1,17 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable()
 export class AuthenticationService {
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private cookies: CookieService) {}
 
-    login(username: string, password: string) {
-        console.log("To be implemented");
+    login(username: string, password: string){
+        let url: string = environment.apiEndpoint + environment.apiSuffix + 'users/login';
+        
+        let body = {
+            Username: username,
+            Password: password
+        };
+
+        this.http.post<any>(url, body)
+        .subscribe(
+            response => {
+                if(response.user && response.token) {
+                    let currentUser = {
+                        id: response.user.id,
+                        username: response.user.userName,
+                        token: response.token
+                    };
+                    this.cookies.set(environment.userRepo, JSON.stringify(currentUser));
+                }
+            },
+            err => console.log(err)
+        );
     }
 
     isAuthenticated(): boolean {
-        let user = localStorage.getItem(environment.userRepo);
+        let user = this.cookies.get(environment.userRepo);
 
         if(user) {
             return true;
@@ -21,6 +43,6 @@ export class AuthenticationService {
     }
 
     logout() {
-        localStorage.removeItem(environment.userRepo);
+        this.cookies.delete(environment.userRepo);
     }
 }
